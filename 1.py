69a6b331140ed752ccfccd7f647c4c541550dfd6
goodzee <38693283+goodzee@users.noreply.github.com>
2018-06-07 12:03:16 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,16 +1,15 @@
 import random
 import time
-n=int(input("enter the number of elements"))
-aList = [random.randint(0,100) for i in range(n)] 
+n=int(input("enter the number of elements")) 
+aList=[random.randint(0,100) for i in range(n)] 
 print(aList)
 start=time.clock()
 for i in range(0, n-1): 
-  least = i
-  for k in range(i+1, n): 
-    if aList[k] < aList[least]:
-      least = k
-  aList[least],aList[i]=aList[i],aList[least] 
+	least=i
+	for k in range(i+1, n): 
+		if aList[k]<aList[least]:
+			least=k 
+	aList[least],aList[i]=aList[i],aList[least] 
 stop=time.clock()
 print(aList) 
 print(stop-start)
-   
